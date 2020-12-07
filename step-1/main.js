cf30b3aea0a7a49ba2ff7f676d1b3f3ccbbd3899
jjunyjjuny <jjuny546@gmail.com>
2020-12-08 06:16:37 +0900

    Write isString() checkIsCorrectInput에서 사용할 문자열인지 판별하는 메소드. 처음엔 문자열인지 확인하고, '123'같은 숫자형 문자열인지 확인하고, 마지막으로 숫자, 특수문자가 섞인 문자열인지 확인한다 세번째 방법으로 앞에 두 가지를 모두 확인할 수 있지만 흔한 케이스에서 문제를 잡는다면 쓸데없이 반복문을 돌리지 않아도 되기에 앞에서 먼저 거르기로 한다

--- step-1/main.js
+++ step-1/main.js
@@ -22,9 +22,32 @@ const convertTextToArray = (text) => {
 };
 
 const checkIsCorrectInput = (array) => {
+  if (array.length !== 3) return false;
+  const [string, num, direction] = array;
+};
+
+const isString = (string) => {
+  //string이 문자열인지
+  if (typeof string !== "string") return false;
+  // '123'과 같은 숫자형 문자열인지
+  if (!isNaN(string)) return false;
+  // '123+ABC'와 같은 문자 외에 숫자, 특수문자가 혼합된 문자열인지
+  const temp_arr = string.split("");
+  for (let i = 0; i < temp_arr.length; i++) {
+    const char = temp_arr[0].charCodeAt(0);
+    if (!((char >= 65 && char < 91) || (char >= 97 && char < 123))) {
+      return false;
+    }
+  }
   return true;
 };
 
+const isNum = (num) => {
+  const number = Number(num);
+  if (!Number.isInteger(number)) return false;
+};
+
+const isDirection = (direction) => {};
 const showResult = () => {
   console.log("result :");
 };
