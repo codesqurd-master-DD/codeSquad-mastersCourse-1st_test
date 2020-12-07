3edafd3810c315217d0e44567f5647a55e03f619
jjunyjjuny <jjuny546@gmail.com>
2020-12-08 04:42:36 +0900

    Set main.js 예상 순서도를 보며 필요하다고 생각되는 함수들을 선언했습니다.

---
+++ step-1/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const btn_submit = document.getElementById("submit");
+btn_submit.addEventListener("click", startGame);
+
+
+const startGame = () => {
+    
+};
+
+const getText = () => {
+  return docuemnt.getElementById("inputedText").value;
+};
+
+const convertTextToArray = (text) => {
+    return array
+};
+
+const checkIsCorrectInput = (array) => {
+    return true
+}
+
+const showResult = () => {
+    console.log('result :')
+}
+
+const pushString = (array) => {
+    
+}
+const calculateVector = (num, direction) => {
+    return vector
+}
