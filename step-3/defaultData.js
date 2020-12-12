875aab384ad6fdcbc97d03e5f968a0334fd6ff1b
jjunyjjuny <jjuny546@gmail.com>
2020-12-13 04:31:52 +0900

    Refactor under 20line 배열 요소가 많아 자동으로 하나씩 줄바꿈이 되거나, 룰 설명 부분에서 긴 내용들이 20라인을 넘게 되는데 이것도 요구사항에 위배되는지는 사실 잘 모르겠지만 일단 처리함.

--- step-3/defaultData.js
+++ step-3/defaultData.js
@@ -45,5 +45,42 @@ const COMMANDS = {
     direction: "cw",
   },
 };
+const RULE = `
 
-module.exports = { DEFAULT_CUBE, COMMANDS };
+      루빅스 큐브 게임에 오신걸 환영합니다!
+          
+       B B B  
+       B B B
+       B B B
+
+W W W  O O O  G G G  Y Y Y 
+W W W  O O O  G G G  Y Y Y 
+W W W  O O O  G G G  Y Y Y 
+ 
+       R R R 
+       R R R 
+       R R R 
+  
+      * 게임 설명 *
+
+        U
+      L F R B
+        D
+
+      > U  Up side을 시계방향으로 90도 회전
+      > F  Front side을 시계방향으로 90도 회전
+      > L  Left side을 시계방향으로 90도 회전 
+      > R  Right side을 시계방향으로 90도 회전
+      > B  Back side을 시계방향으로 90도 회전
+      > D  Down side을 시계방향으로 90도 회전
+      > M  다시 섞기
+      > Q  프로그램을 종료한다.
+      
+      문자에 ' 를 붙이면 반시계으로 90도 회전, 숫자 2 를 붙이면 2번 회전(180도) 합니다! 
+      (3번 돌리기보단 반시계로 돌리는게 낫겠죠?)
+
+      한 번에 여러 명령을 입력할 수 있어요!
+      EX) UU'RL2B
+      `;
+
+module.exports = { DEFAULT_CUBE, COMMANDS, RULE };
